0bd663ccbdf2105991dd519c490c68961e076770
Michael Conrad <michael@threepears.com>
2016-02-22 11:17:02 -0600

    Add Redis, build middleware for Analytics counting

--- server.js
+++ server.js
@@ -2,14 +2,32 @@
 
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
 
 const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || "supersecret";
 
 app.set("view engine", "jade");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: SESSION_SECRET,
+  store: new RedisStore()
+}));
+
+app.use((req, res, next) => {
+  req.session.visits = req.session.visits || {};
+  req.session.visits[req.url] = req.session.visits[req.url] || 0;
+  req.session.visits[req.url]++;
+
+  //req.session.count = req.session.count || 0;
+  //req.session.count++;
+  console.log(req.session);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("index");

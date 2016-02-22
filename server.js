e5d566bc6694acf5ad7b72c61dcc430b0d378b52
Michael Conrad <michael@threepears.com>
2016-02-22 09:46:44 -0600

    Add views, body parser, login routes

--- server.js
+++ server.js
@@ -3,13 +3,25 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "jade");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.send("Node!!!");
+  res.render("index");
 });
 
-app.set("view engine", "jade");
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);

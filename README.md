e7dd2c4dc8c4975b3418570fe42e58cc2b3cb436
Michael Conrad <michael@threepears.com>
2016-02-26 23:51:35 -0600

    Add views for register and login pages, finish login process, edit README

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # Node Authentication
 
-Having fun with Node.  And authentication.
+A NSS classroom project working with authentication in Node using Passport
+
+### Goal
+
+Create a simple register and login process with authentication using Passport and BCrypt. Also use in-memory data storage with Redis.
+
+### Tools Used
+
+Node, Express, Passport, BCRypt, Redis, Jade, MongoDB, Mongoose, Nodemon, Code editor
+
+### Installation
+
+Create new repository and enter the following command:
+
+```sh
+$ git clone https://github.com/threepears/node-authentication.git
+```
+
+Run the following command to install all dependencies:
+
+```sh
+$ npm install
+```
+
+Now start your Mongo server in Terminal with this command:
+
+```sh
+$ mongod
+```
+
+Then start your regular Node server with this Terminal command:
+
+```sh
+$ npm run start
+```
+
+Now open your browser and go to http://localhost:3000/ to start and use the application!
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
 const RedisStore = require("connect-redis")(session);
 const methodOverride = require("method-override");
 
@@ -17,6 +19,8 @@ const SESSION_SECRET = process.env.SESSION_SECRET || "supersecret";
 
 app.set("view engine", "jade");
 
+app.locals.title = '';
+
 app.use(methodOverride("_method"));
 
 app.use(session({
@@ -24,21 +28,38 @@ app.use(session({
   store: new RedisStore()
 }));
 
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 app.use((req, res, next) => {
-  req.session.visits = req.session.visits || {};
-  req.session.visits[req.url] = req.session.visits[req.url] || 0;
-  req.session.visits[req.url]++;
+  // req.session.visits = req.session.visits || {};
+  // req.session.visits[req.url] = req.session.visits[req.url] || 0;
+  // req.session.visits[req.url]++;
+
+  res.locals.user = req.user;
 
   //req.session.count = req.session.count || 0;
   //req.session.count++;
-  console.log(req.session);
+  console.log(req.user);
+  console.log(res.locals);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  console.log(res.locals);
   next();
 });
 
 app.use(userRoutes);
 
 app.get("/", (req, res) => {
-  res.render("index", { user: req.session.user });
+  // res.render("index", { user: req.session.user });
+  res.render("index");
 });
 
 mongoose.connect('mongodb://localhost:27017/nodeauth', (err) => {
